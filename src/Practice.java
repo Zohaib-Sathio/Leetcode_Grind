64835c6ad08dc15f3325bc5abd5fe49cb334faf7
Zohaib-Sathio <zohaibhussainsathio@gmail.com>
2022-08-24 16:17:15 +0500

    re-organized Practice java class

--- src/Practice.java
+++ src/Practice.java
@@ -2,11 +2,8 @@ import java.util.HashMap;
 
 public class Practice {
 
-}
-
-class Solution {
     public int firstUniqChar(String s) {
-        HashMap<Character, Integer> map = new HashMap();
+        HashMap<Character, Integer> map = new HashMap<>();
         for(int i = 0; i < s.length(); i++){
             map.put(s.charAt(i), 0);
         }
@@ -21,5 +18,9 @@ class Solution {
             }
         }
         return -1;
+    }
+
+    public static void main(String[] args) {
+
     }
 }
