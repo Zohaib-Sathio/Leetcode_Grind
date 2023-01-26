f3cc1c844de107811cd275e7690a692abeec3525
Zohaib Hussain <zohaibhussainsathio@gmail.com>
2023-01-26 09:42:08 +0500

    Committed

---
+++ src/HARD_Problems/ListNode.java
@@ -0,0 +1,11 @@
+package HARD_Problems;
+
+public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(){
+        }
+        ListNode(int val){
+            this.val = val;
+        }
+    }
