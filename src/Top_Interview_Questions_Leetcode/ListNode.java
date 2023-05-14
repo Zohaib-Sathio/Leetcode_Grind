bcee4e3a23305f8d75860d79f14d75296a1d3e18
Zohaib Hussain <zohaibhussainsathio@gmail.com>
2023-05-14 08:14:04 +0500

    base classes added

---
+++ src/Top_Interview_Questions_Leetcode/ListNode.java
@@ -0,0 +1,11 @@
+package Top_Interview_Questions_Leetcode;
+
+public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(){
+        }
+        ListNode(int val){
+            this.val = val;
+        }
+    }
