af1372629ddda303190bca57a5331f4d98dac747
Zohaib Hussain <zohaibhussainsathio@gmail.com>
2023-06-05 09:08:56 +0500

    Solved Problem

---
+++ src/BiWeeklyContest/BuyTwoChocolates.java
@@ -0,0 +1,11 @@
+package BiWeeklyContest;
+
+import java.util.Arrays;
+
+public class BuyTwoChocolates {
+    public int buyChoco(int[] prices, int money) {
+        Arrays.sort(prices);
+        if(prices[0] + prices[1] > money) return money;
+        return money - (prices[0] + prices[1]);
+    }
+}
